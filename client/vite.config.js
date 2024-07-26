0986ff1e078e182f2736152a13e59e299db6c08a
Mantas Imbrasas <mantasimbrasas@gmail.com>
2024-07-26 16:25:50 +0300

    change build output directory to suit vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import svgr from "vite-plugin-svgr"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
   build: {
     chunkSizeWarningLimit: 1600,
+    outDir: "build",
   },
-})
+});
